5c541a74a2be2d493a933db76a6166a32f82c40b
Dinidu <dinidu01@gmail.com>
2020-01-01 20:11:00 -0600

    integrated bootstrap theme

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,6 +1,13 @@
 class AssetsController < ApplicationController
 
   def index
+    # TODO Add code to load all assets
+    # @assets = Asset.all
+  end
+
+  def new
+    # TODO add code to add a new asset
+    # @asset = Asset.new
   end
   
 end
