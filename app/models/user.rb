8af1a7f4419bc27678822e2007986b2d7c9fcf7e
Candace Van Doren <cvd2586@gmail.com>
2020-01-02 00:15:21 +0000

    add top nav and footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :assets 
+  has_many :assets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
